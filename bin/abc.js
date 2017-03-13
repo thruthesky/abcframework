b4d0acb311f0e20d9d0854feb0ee28a0a3b6d86b
JaeHo Song <thruthesky@JaeHos-MacBook-Pro.local>
2017-03-14 01:11:41 +0800

    work on ng serve

--- bin/abc.js
+++ bin/abc.js
@@ -3,37 +3,88 @@
 'use strict';
 
 
+const fs = require('fs-extra')
 var argv = require('yargs').argv;
-if ( argv._[0] == 'new' ) {
-    console.log("ng new")
-    
-}
-else if ( argv._[0] == 'serve' ) {
-    console.log('ng serve');
-}
-
-/*
-console.log(argv.theme);
-console.log(argv._);
-*/
-
+var shell = require('shelljs');
 
 const spawn = require('child_process').spawn;
-const ls = spawn('ng', [ 'new', argv._[1], '--verbose'] );
-
-ls.stdout.on('data', (data) => {
-  print( data );
-});
-
-ls.stderr.on('data', (data) => {
-  print( data );
-});
-
-ls.on('close', (code) => {
-  print( data );
-});
 
 
-function print( str ) {
-    process.stdout.write( str );
+
+// Provide a title to the process in `abc`
+process.title = 'abc';
+
+
+if ( argv._[0] == 'new' ) {
+    install_abc();
+}
+
+
+
+
+function install_abc() {
+    let dst = argv._[1];
+    let src = __dirname + '/../angular/v2.4';
+
+    display_message(`Installing ABC project on ${dst}`);
+    display_message(`Copying ${src} to ${dst}`);
+    try {
+        fs.copySync(src, dst);
+        display_message("success!")
+    } catch (err) {
+        console.error(err)
+    }
+
+    display_message(`Change Directory: ${dst}`);
+    // shell.cd( dst );
+    process.chdir( dst );
+    npm_install( code => {
+        display_message("npm install: done");
+        console.log('');
+        display_message("Going to run ng serve");
+        ng_serve( code => {
+
+        });
+    } );
+}
+
+
+function npm_install(callback) {
+
+    let ls = spawn('npm', ['install', '--verbose']);
+
+    ls.stdout.on('data', (data) => {
+        display_message(`${data}`);
+    });
+
+    ls.stderr.on('data', (data) => {
+        display_message(`${data}`);
+    });
+
+    ls.on('close', (code) => {
+        display_message(`child process exited with code ${code}`);
+        callback( code );
+    });
+}
+
+function ng_serve( callback ) {
+
+    let ls = spawn('ng', ['serve']);
+
+    ls.stdout.on('data', (data) => {
+        display_message(`${data}`);
+    });
+
+    ls.stderr.on('data', (data) => {
+        display_message(`${data}`);
+    });
+
+    ls.on('close', (code) => {
+        display_message(`child process exited with code ${code}`);
+        callback( code );
+    });
+}
+
+function display_message( msg ){
+    process.stdout.write( msg );
 }
