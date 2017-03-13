dc9786a373fd7170a0d75dd8d4c77e62446fd861
JaeHo Song <thruthesky@JaeHos-MacBook-Pro.local>
2017-03-13 22:17:38 +0800

    removing lodash

--- bin/abc.js
+++ bin/abc.js
@@ -2,4 +2,38 @@
 
 'use strict';
 
-console.log("ABC 0.0.1");
+
+var argv = require('yargs').argv;
+if ( argv._[0] == 'new' ) {
+    console.log("ng new")
+    
+}
+else if ( argv._[0] == 'serve' ) {
+    console.log('ng serve');
+}
+
+/*
+console.log(argv.theme);
+console.log(argv._);
+*/
+
+
+const spawn = require('child_process').spawn;
+const ls = spawn('ng', [ 'new', argv._[1], '--verbose'] );
+
+ls.stdout.on('data', (data) => {
+  print( data );
+});
+
+ls.stderr.on('data', (data) => {
+  print( data );
+});
+
+ls.on('close', (code) => {
+  print( data );
+});
+
+
+function print( str ) {
+    process.stdout.write( str );
+}
